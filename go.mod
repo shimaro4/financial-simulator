9ba36c3a87fe8ac1834114e047fd6942dd5a4b64
shimaro4 <martianshiv@gmail.com>
2025-08-19 20:58:43 -0400

    Add all struct definitions with corrected field names

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shimaro4/financial-simulator
+
+go 1.24.3
---
+++ internal/models.go
@@ -0,0 +1,40 @@
+package internal
+
+type Income struct {
+	Earner string
+	Source string
+	Amount float64
+	YearlyAvgIncrease float64
+}
+
+type Expense struct {
+	Housing float64
+	HousingType string
+	Groceries float64
+	Utilities float64
+	Miscl float64
+}
+
+type Savings struct {
+	KidsEducation float64
+	Entertainment float64
+	RainyDay float64
+	RepairMaint float64
+}
+
+type Investment struct {
+	MonthlyContributions float64
+	TotalInvested float64
+	Balance float64
+	ReturnRate float64
+}
+
+type Household struct {
+	Name string
+	NumPeople int
+	State string
+	Incomes []Income
+	Expense Expense
+	Savings Savings
+	Investment Investment
+}
